86cdd2def20dd5d627108350c8b6d4aa77119d37
Andrey Egorov <andreyegorov1985@gmail.com>
2019-11-14 23:37:43 +0300

    second commit -very bad and unstable code

--- client.py
+++ client.py
@@ -2,20 +2,42 @@ import asyncio
 import websockets
 from common import enc
 
-SERVER_ADDR = input('Введите IP-адрес сервера:\n')
+SERVER_ADDR = '127.0.0.1'
+name = input("Введите ваше имя:\n")
 
-async def hello():
+
+async def register(websocket, name):
+    msg = {'name': name}
+    msg = enc.encode(msg)
+    await websocket.send(msg)
+    resp = await websocket.recv()
+    resp = enc.decode(resp)
+    if resp['authorization'] == 'success':
+        return True
+
+    return False
+
+
+async def chat(websocket, name):
+    while True:
+        msg = {'name': name}
+        payload = input(f"{name}: ")
+        msg['payload'] = payload
+        await websocket.send(enc.encode(msg))
+        resp = await websocket.recv()
+        response = enc.decode(resp)
+        print(f"{response['payload']}")
+
+
+async def main(name):
     uri = f"ws://{SERVER_ADDR}:8765"
     async with websockets.connect(uri) as websocket:
-        msg = {}
-        name = input("Введите ваше имя:\n")
-        while True:
-            msg['name'] = name
-            payload = input("Введите ваше сообщение:\n")
-            msg['payload'] = payload
-            await websocket.send(enc.encode(msg))
-            resp = await websocket.recv()
-            response = enc.decode(resp)
-            print(f"{response['payload']}")
+        reg_status = await register(websocket, name)
+        if reg_status:
+            await chat(websocket, name)
+        else:
+            return -1
 
-asyncio.get_event_loop().run_until_complete(hello())
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main(name))
--- server.py
+++ server.py
@@ -3,19 +3,55 @@ import websockets
 import datetime
 from common import enc
 
+USERS = {}
 
-async def hello(websocket, path):
+
+def register_user(websocket, name):
+    if name not in USERS:
+        USERS[name] = websocket
+        return True
+    return False
+
+
+def format_message(message):
     response = {}
+    current_time = datetime.datetime.today().strftime("%Y-%m-%d %H.%M.%S")
+    response['payload'] = f"{current_time} {message['name']}: {message['payload']}"
+    return enc.encode(response)
+
+
+async def send_msg_all_users(message):
+    if USERS:
+        response = format_message(message)
+        await asyncio.wait([user.send(response) for user in USERS.values()])
+
+
+async def send_success(websocket):
+    response = enc.encode({'authorization': 'success'})
+    await websocket.send(response)
+
+
+async def chatting(websocket):
     while True:
         received = await websocket.recv()
         message = enc.decode(received)
-        current_time = datetime.datetime.today().strftime("%Y-%m-%d %H.%M.%S")
-        response['payload'] = f"{current_time} {message['name']}: {message['payload']}"
-        resp = enc.encode(response)
-        await websocket.send(resp)
+
+        if message['payload'] == r'/quit':
+            break
+
+        await send_msg_all_users(message)
 
 
-start_server = websockets.serve(hello, "localhost", 8765)
+async def main(websocket, path):
+    received = await websocket.recv()
+    message = enc.decode(received)
+    username = message['name']
+    if register_user(websocket, username):
+        await send_success(websocket)
+    await chatting(websocket)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+
+if __name__ == '__main__':
+    start_server = websockets.serve(main, "localhost", 8765)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
